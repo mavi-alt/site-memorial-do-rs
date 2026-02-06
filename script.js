b270a3a911b64a66d02b43d92642a8b9db114836
manu <manuelavialmeida@hotmail.com>
2026-02-06 13:25:30 -0300

    inicio de menu hamburguer

--- script.js
+++ script.js
@@ -5,3 +5,56 @@ const search = document.querySelector('.input-wrapper');
 icon.onclick = function() {
     search.classList.toggle('active');
 }
+
+// Função do menu hambúrguer
+
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const menu = document.getElementById("menu");
+
+hamburgerBtn.classList.add("hamburger-btn-js-enabled");
+
+function closeMenu() {
+    console.log(this);
+    hamburgerBtn.setAttribute("aria-expanded", "false");
+    menu.setAttribute("aria-hidden", "true");
+    menu.classList.add("menu-closed");
+};
+
+closeMenu();
+
+hamburgerBtn.addEventListener("click", function(e){
+    
+    let expanded = this.getAttribute("aria-expanded") === "true" ? true : false;
+    document.removeEventListener("click", closeMenu);
+
+    if (expanded) {
+        menu.classList.add("menu-closed");
+    } else {
+        menu.classList.remove("menu-closed");
+    };
+
+    this.setAttribute("aria-expanded", !expanded);
+    menu.setAttribute("aria-hidden", expanded);
+
+    setTimeout(function(){if (!expanded) {
+        document.addEventListener("click", closeMenu);
+    }}, 1)
+
+});
+
+const mediaQuery = window.matchMedia("(min-width: 960px)")
+
+function handleMediaqueryChange(e){
+    if (e.matches) {
+        menu.setAttribute("aria-hidden", "false");
+        hamburgerBtn.setAttribute("aria-expanded", "true");
+        menu.classList.remove("menu-closed");
+    } else {
+        menu.setAttribute("aria-hidden", "true");
+        hamburgerBtn.setAttribute("aria-expanded", "false");
+        menu.classList.add("menu-closed");
+    }
+}
+
+mediaQuery.addEventListener("change", handleMediaqueryChange);
+handleMediaqueryChange(mediaQuery);
